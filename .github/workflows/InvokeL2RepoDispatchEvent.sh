d9c8bc4478fb7c182ab7c2757f892a3bfee3f40f
Jyotsna <Josh-01@github.com>
2021-03-25 20:08:45 +0530

    Update InvokeL2RepoDispatchEvent.sh

--- .github/workflows/InvokeL2RepoDispatchEvent.sh
+++ .github/workflows/InvokeL2RepoDispatchEvent.sh
@@ -22,7 +22,6 @@ getPayLoad() {
 EOF
 }
 
-
 code=$(curl -X GET https://github.com/login/oauth/authorize?client_id=9e4fedaa995f29228ee)
 access_code=$(curl -H "Accept: application/json" -X POST https://github.com/login/oauth/access_token?client_id=9e4fedaa995f29228ee&client_secret=ef8c49d9267f66a2278a5007fc6d9ac5f2e605ce&code=$code)
 
