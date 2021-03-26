45f52a46258e1f0beb17c7e7ed6f544f54c2c8d2
Jyotsna <Josh-01@github.com>
2021-03-26 14:36:04 +0530

    Update InvokeL2RepoDispatchEvent.sh

--- .github/workflows/InvokeL2RepoDispatchEvent.sh
+++ .github/workflows/InvokeL2RepoDispatchEvent.sh
@@ -23,7 +23,18 @@ EOF
 }
 
 code=$(curl -X GET https://github.com/login/oauth/authorize?client_id=bd3c1ac33bd049bb)
-access_code=$(curl -X POST https://github.com/login/oauth/access_token?client_id=bd3c1ac33bd049bb&client_secret=7c4ee5d0f179629da3e7a52012abcb969ea08193&code=$code)
+
+getCreds() {
+cat <<EOF
+{
+    "client_id" : bd3c1ac33bd049bb,
+    "client_secret" : 7c4ee5d0f179629da3e7a52012abcb969ea08193,
+    "code: : ${code}
+}
+EOF
+}
+
+access_code=$(curl -X POST https://github.com/login/oauth/access_token --data "$(getCreds)")
 
 echo ${access_code}
 
@@ -31,10 +42,8 @@ access_token=${access_code:13:40}
 response=""
 
 if [ "$access_code" == ""  ]; then
-    echo ${access_code}
     echo "access_code is empty"
 else
-    echo ${access_code}
     echo "access_code is not empty"
     response=$(curl -H "Authorization: token $access_token" -X POST https://api.github.com/repos/Azure/azure-actions-integration-tests/dispatches --data "$(getPayLoad)")
 fi
